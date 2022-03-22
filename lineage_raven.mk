fb772feaa5056aa4355e68a0f49c025149c4f80b
Chirayu Desai <chirayudesai1@gmail.com>
2022-03-22 14:38:39 +0530

    Update build fingerprint and desc to March 2022 (12L)
    Change-Id: I081abbdc7da60729fa02071301533079b3af1e94

--- lineage_raven.mk
+++ lineage_raven.mk
@@ -16,9 +16,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1 7888514 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220305.013.A3 8229987 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.211205.016.A1/7888514:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SP2A.220305.013.A3/8229987:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
