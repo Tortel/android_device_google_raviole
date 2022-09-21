4bfd5cbfd5782efe4cc8ca61dd8964e2e1c0a750
Scott Warner <Tortel1210@gmail.com>
2022-09-21 13:45:46 -0400

    Set kernel src/config, override Lineage kernel build
    Setting TARGET_NO_KERNEL_OVERRIDE skips the entire vendor/lineage kernel.mk
    There is a duplicate copy command generated in the lineage kernel.mk, so jsut skip it
    
    Change-Id: Ia1fe875fb49d59e62af6e974ce06637c26f3dda4

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,2 +1,8 @@
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/google/raviole/lineage_compatibility_matrix.xml
+
+# Kernel stuff
+TARGET_KERNEL_CONFIG := slider_gki_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/raviole/private/gs-google
+
+TARGET_NO_KERNEL_OVERRIDE := true
